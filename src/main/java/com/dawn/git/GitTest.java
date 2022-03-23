79a8458f5ead3e4bb3a9cd5b2514447254bfe4d9
Dawn <dawn@133.com>
2022-03-23 16:39:44 +0800

    second commit

--- src/main/java/com/dawn/git/GitTest.java
+++ src/main/java/com/dawn/git/GitTest.java
@@ -3,5 +3,7 @@ package com.dawn.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
+        System.out.println("hello git!3");
     }
 }
